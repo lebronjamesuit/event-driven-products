8f515e1363055d28bf6ec330efdc365d03539319
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-07-19 15:35:03 +0100

    Init Shared Common Project to inject dependency.

---
+++ sharedCommon/src/main/java/org/msss/cqrs/saga/sharedcommon/command/ReserveProductCommand.java
@@ -0,0 +1,18 @@
+package org.msss.cqrs.saga.sharedcommon.command;
+
+
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+public class ReserveProductCommand {
+
+    @TargetAggregateIdentifier
+    private final String productId;
+    private final int quantity;
+    private final String orderId;
+    private final String userId;
+
+}
